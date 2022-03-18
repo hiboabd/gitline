8c05eb3f897c3b5f419faf1f32fbba8b8c6dc01d
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-03-18 13:28:48 +0000

    Fix asset handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,57 @@
-var path = require('path')
-var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: {
-    all: './web/assets/main.js'
-  },
-  mode: 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              url: false,
+    entry: {
+        all: './web/assets/main.js'
+    },
+    mode: 'production',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
             },
-          },
-          {
-            loader: 'sass-loader'
-          }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            // {
+            //   test: /\.svg$/,
+            //   use: 'file-loader'
+            // }
         ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
+    },
+    output: {
+        filename: 'javascript/[name].js',
+        path: path.resolve(__dirname, 'web/static')
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                  from: path.resolve(__dirname, "web/assets/images"),
+                  to: path.resolve(__dirname, "web/static/images")},
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'stylesheets/[name].css'
+        }),
     ]
-  },
-  output: {
-    filename: 'javascript/[name].js',
-    path: path.resolve(__dirname, 'web/static')
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'stylesheets/[name].css'
-    }),
-  ]
 }
